fcb8d091b0c889fcf0004243002aa2f4236a06b3
Kevyn Gonçalves <kevynredlotus@gmail.com>
2023-01-29 22:56:08 -0300

    fix: adds await before MessageMedia

--- src/server.js
+++ src/server.js
@@ -90,7 +90,7 @@ client.on("message_create", async (incomingMessage) => {
         if (incomingMessage.type === 'image') {
             try {
                 const { data } = await incomingMessage.downloadMedia();
-                const image = new MessageMedia("image/jpeg", data, "image/jpg");
+                const image = await new MessageMedia("image/jpeg", data, "image/jpg");
                 await client.sendMessage(sender, image, {sendMediaAsSticker: true});
             } catch (error) {
                 console.error("Deu ruim em processar essa imagem enviada", error);
@@ -101,7 +101,7 @@ client.on("message_create", async (incomingMessage) => {
                 const url = msg.body.substring(msg.body.indexOf(" ")).trim()
                 const { data } = await axios.get(url, {responseType: 'arraybuffer'})
                 const returnedB64 = Buffer.from(data).toString('base64');
-                const image = new MessageMedia("image/jpeg", returnedB64, "image.jpg")
+                const image = await new MessageMedia("image/jpeg", returnedB64, "image.jpg")
                 await client.sendMessage(sender, image, { sendMediaAsSticker: true })
             } catch (error) {
                 console.error("Deu ruim em processar essa imagem enviada pela URL");
